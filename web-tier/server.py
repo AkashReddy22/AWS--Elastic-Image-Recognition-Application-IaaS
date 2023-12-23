ae50e0f5f8796158d3d3ba761311f1f2b935f77b
AkashReddy22 <amaligir@asu.edu>
2023-12-22 20:48:43 -0700

    AWs EC2 and S3

---
+++ web-tier/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from queue_manager import SQSQueueManager
+from gevent import monkey
+monkey.patch_all()
+
+app = Flask(__name__)
+
+queue_manager = SQSQueueManager()
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'myfile' not in request.files or request.files['myfile'] == '':
+        return jsonify({'error': 'No image provided'}), 400
+    image = request.files['myfile']
+    filename = image.filename
+    messageId = queue_manager.handle_request(image, filename)
+    result = queue_manager.get_result(messageId)
+    if result is None:
+        return "Invalid MessageId", 400
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=False)
